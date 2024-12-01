c4c8356b83cb66b2c47af5a71826e3057388cb9d
leydev <araujowesley@proton.me>
2024-11-30 22:30:27 -0300

    fix report descriptor

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+
 use std::num::NonZero;
 
 use esp32_nimble::{
@@ -11,8 +12,10 @@ use esp_idf_sys::{self as _};
 
 mod report_descriptor;
 
-const STRUM_UP: u8 = 0x51;
-const STRUM_DOWN: u8 = 0x52;
+const STRUM: u8 = 0x51;
+const ENTER: u8 = 0x28;
+const STRUM_UP: u8 = 0x52;
+const STRUM_DOWN: u8 = 0x51;
 const Q: u8 = 0x14;
 const W: u8 = 0x1a;
 const E: u8 = 0x08;
@@ -22,17 +25,23 @@ const H: u8 = 0x0b;
 const NULL: u8 = 0x00;
 
 #[repr(packed)]
-struct GamepadReport {
-    // Gamepad buttons (16 buttons)
-    pub buttons: u8, // 16 buttons, represented by a bitmask
-                     // Gamepad axes (2 axes: X, Y)
-                     // pub x_axis: u8, // X axis value (0-127)
-                     // pub y_axis: u8, // Y axis value (0-127)
+struct Report {
+    pub buttons: u8,
+}
+
+fn generate_report(buttons: &[bool; 8], report: &mut Report) {
+    println!("{:08b}", report.buttons);
+    for (i, &button) in buttons.iter().enumerate() {
+        if button {
+            report.buttons |= 1 << i;
+        } else {
+            report.buttons &= !(1 << i);
+        }
+    }
 }
 
 fn main() {
-    let _ = BLEDevice::set_device_name("GuittarPad").unwrap();
-
+    esp_idf_sys::link_patches();
     let ble_device = BLEDevice::take();
 
     ble_device
@@ -48,19 +57,19 @@ fn main() {
     });
 
     server.on_disconnect(|_c, _r| {
-        println!("Disconnected {:#?} | reason: {:#?}", _c, _r.err().unwrap());
+        println!("Deconnected {:#?} | reason: {:#?}", _c, _r.err().unwrap());
     });
 
     let mut hid = BLEHIDDevice::new(server);
 
-    let input_gamepad = hid.input_report(report_descriptor::GAMEPAD_ID);
-    let _output_gamepad = hid.output_report(report_descriptor::GAMEPAD_ID);
+    let input_keyboard = hid.input_report(report_descriptor::KEYBOARD_ID);
+    let _output_keyboard = hid.output_report(report_descriptor::KEYBOARD_ID);
 
     hid.manufacturer("Leydev");
     hid.pnp(0x02, 0x05ac, 0x820a, 0x0210);
     hid.hid_info(0x00, 0x01);
 
-    hid.report_map(report_descriptor::GAMEPAD_REPORT_DESCRIPTOR);
+    hid.report_map(report_descriptor::HID_REPORT_DISCRIPTOR);
 
     hid.set_battery_level(100);
 
@@ -78,13 +87,9 @@ fn main() {
 
     ble_advertising.lock().start().unwrap();
 
-    let mut gamepad_report = GamepadReport {
-        buttons: 0, // Start with all buttons released (0)
-                    // x_axis: 0,  // Default X axis value (center position)
-                    // y_axis: 0,  // Default Y axis value (center position)
-    };
+    let mut keys_report = Report { buttons: 0x00 };
 
-    // MAP BUTTONS
+    // MAP KEY
     let peripherals = Peripherals::take().unwrap();
 
     let mut pick_down_btn = PinDriver::input(peripherals.pins.gpio33).unwrap();
@@ -108,6 +113,7 @@ fn main() {
     start_btn.set_pull(Pull::Up).unwrap();
 
     // SETUP BUTTON INTERRUPT
+
     pick_down_btn
         .set_interrupt_type(InterruptType::AnyEdge)
         .unwrap();
@@ -124,6 +130,7 @@ fn main() {
         .unwrap();
 
     let notification = Notification::new();
+    let mut buttons: [bool; 8] = [false; 8];
 
     loop {
         let waker_1 = notification.notifier();
@@ -188,74 +195,35 @@ fn main() {
         l1_btn.enable_interrupt().unwrap();
         start_btn.enable_interrupt().unwrap();
 
-        // Update the gamepad report based on button presses
-        gamepad_report.buttons = 0; // Reset button states
+        buttons[0] = cro_btn.is_low();
+        buttons[1] = cir_btn.is_low();
+        buttons[2] = srq_btn.is_low();
+        buttons[3] = tri_btn.is_low();
+        buttons[4] = l1_btn.is_low();
+        buttons[5] = pick_up_btn.is_low();
+        buttons[6] = pick_up_btn.is_low();
+        buttons[7] = start_btn.is_low();
 
-        if pick_up_btn.is_low() {
-            gamepad_report.buttons |= 0x0001; // Set button 1 (Pick up)
-        }
-        if pick_down_btn.is_low() {
-            gamepad_report.buttons |= 0x0002; // Set button 2 (Pick down)
-        }
-        if cro_btn.is_low() {
-            gamepad_report.buttons |= 0x0004; // Set button 3 (Cro)
-        }
-        if cir_btn.is_low() {
-            gamepad_report.buttons |= 0x0008; // Set button 4 (Cir)
-        }
-        if srq_btn.is_low() {
-            gamepad_report.buttons |= 0x0010; // Set button 5 (Srq)
-        }
-        if tri_btn.is_low() {
-            gamepad_report.buttons |= 0x0020; // Set button 6 (Tri)
-        }
-        if l1_btn.is_low() {
-            gamepad_report.buttons |= 0x0040; // Set button 7 (L1)
-        }
-        if start_btn.is_low() {
-            gamepad_report.buttons |= 0x0080; // Set button 8 (Start)
-        }
+        generate_report(&buttons, &mut keys_report);
 
-        // Optionally, you can add joystick axis logic here:
-        // gamepad_report.x_axis = 127; // Example: Centered X axis
-        // gamepad_report.y_axis = 127; // Example: Centered Y axis
-
-        // Send the gamepad report
-        input_gamepad.lock().set_from(&gamepad_report).notify();
-
-        print!("{}", gamepad_report.buttons);
+        input_keyboard.lock().set_from(&keys_report).notify();
 
         // Block until any waker.notify() call
         notification.wait_any();
 
-        gamepad_report.buttons = 0; // Reset button states
+        // It check value and send again to partially fix a bug
+        // Some times the "release button" is't sent. So it is for that
+        buttons[0] = cro_btn.is_low();
+        buttons[1] = cir_btn.is_low();
+        buttons[2] = srq_btn.is_low();
+        buttons[3] = tri_btn.is_low();
+        buttons[4] = l1_btn.is_low();
+        buttons[5] = pick_up_btn.is_low();
+        buttons[6] = pick_up_btn.is_low();
+        buttons[7] = start_btn.is_low();
 
-        if pick_up_btn.is_low() {
-            gamepad_report.buttons |= 0x0001; // Set button 1 (Pick up)
-        }
-        if pick_down_btn.is_low() {
-            gamepad_report.buttons |= 0x0002; // Set button 2 (Pick down)
-        }
-        if cro_btn.is_low() {
-            gamepad_report.buttons |= 0x0004; // Set button 3 (Cro)
-        }
-        if cir_btn.is_low() {
-            gamepad_report.buttons |= 0x0008; // Set button 4 (Cir)
-        }
-        if srq_btn.is_low() {
-            gamepad_report.buttons |= 0x0010; // Set button 5 (Srq)
-        }
-        if tri_btn.is_low() {
-            gamepad_report.buttons |= 0x0020; // Set button 6 (Tri)
-        }
-        if l1_btn.is_low() {
-            gamepad_report.buttons |= 0x0040; // Set button 7 (L1)
-        }
-        if start_btn.is_low() {
-            gamepad_report.buttons |= 0x0080; // Set button 8 (Start)
-        }
+        generate_report(&buttons, &mut keys_report);
 
-        // Optionally, you can check again and resend if needed.
-        input_gamepad.lock().set_from(&gamepad_report).notify();
+        input_keyboard.lock().set_from(&keys_report).notify();
     }
 }
--- src/report_descriptor.rs
+++ src/report_descriptor.rs
@@ -1,106 +1,21 @@
 use esp32_nimble::hid::*;
 
-pub const GAMEPAD_ID: u8 = 0x0;
 pub const KEYBOARD_ID: u8 = 0x01;
-pub const MEDIA_KEYS_ID: u8 = 0x02;
 
-pub const KEYBOARD_DESCRIPTOR: &[u8] = hid!(
-    (USAGE_PAGE, 0x01), // USAGE_PAGE (Generic Desktop Ctrls)
-    (USAGE, 0x06),      // USAGE (Keyboard)
-    (COLLECTION, 0x01), // COLLECTION (Application)
-    // ------------------------------------------------- Keyboard
-    (REPORT_ID, GAMEPAD_ID), //   REPORT_ID (0)
-    (USAGE_PAGE, 0x07),      //   USAGE_PAGE (Kbrd/Keypad)
-    (USAGE_MINIMUM, 0xE0),   //   USAGE_MINIMUM (0xE0)
-    (USAGE_MAXIMUM, 0xE7),   //   USAGE_MAXIMUM (0xE7)
-    (LOGICAL_MINIMUM, 0x00), //   LOGICAL_MINIMUM (0)
-    (LOGICAL_MAXIMUM, 0x01), //   Logical Maximum (1)
-    (REPORT_SIZE, 0x01),     //   REPORT_SIZE (1)
-    (REPORT_COUNT, 0x08),    //   REPORT_COUNT (8)
-    (HIDINPUT, 0x02), //   INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    (REPORT_COUNT, 0x01), //   REPORT_COUNT (1) ; 1 byte (Reserved)
-    (REPORT_SIZE, 0x08), //   REPORT_SIZE (8)
-    (HIDINPUT, 0x01), //   INPUT (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    (REPORT_COUNT, 0x05), //   REPORT_COUNT (5) ; 5 bits (Num lock, Caps lock, Scroll lock, Compose, Kana)
-    (REPORT_SIZE, 0x01),  //   REPORT_SIZE (1)
-    (USAGE_PAGE, 0x08),   //   USAGE_PAGE (LEDs)
-    (USAGE_MINIMUM, 0x01), //   USAGE_MINIMUM (0x01) ; Num Lock
-    (USAGE_MAXIMUM, 0x05), //   USAGE_MAXIMUM (0x05) ; Kana
-    (HIDOUTPUT, 0x02), //   OUTPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    (REPORT_COUNT, 0x01), //   REPORT_COUNT (1) ; 3 bits (Padding)
-    (REPORT_SIZE, 0x03), //   REPORT_SIZE (3)
-    (HIDOUTPUT, 0x01), //   OUTPUT (Const,Array,Abs,No Wrap,Linear,Preferred State,No Null Position,Non-volatile)
-    (REPORT_COUNT, 0x07), //   REPORT_COUNT (6) ; 6 bytes (Keys)
-    (REPORT_SIZE, 0x08), //   REPORT_SIZE(8)
-    (LOGICAL_MINIMUM, 0x00), //   LOGICAL_MINIMUM(0)
-    (LOGICAL_MAXIMUM, 0x65), //   LOGICAL_MAXIMUM(0x65) ; 101 keys
-    (USAGE_PAGE, 0x07), //   USAGE_PAGE (Kbrd/Keypad)
-    (USAGE_MINIMUM, 0x00), //   USAGE_MINIMUM (0)
-    (USAGE_MAXIMUM, 0x65), //   USAGE_MAXIMUM (0x65)
-    (HIDINPUT, 0x00),  //   INPUT (Data,Array,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    (END_COLLECTION),  // END_COLLECTION
-    // ------------------------------------------------- Media Keys
-    (USAGE_PAGE, 0x0C),         // USAGE_PAGE (Consumer)
-    (USAGE, 0x01),              // USAGE (Consumer Control)
-    (COLLECTION, 0x01),         // COLLECTION (Application)
-    (REPORT_ID, MEDIA_KEYS_ID), //   REPORT_ID (3)
-    (USAGE_PAGE, 0x0C),         //   USAGE_PAGE (Consumer)
-    (LOGICAL_MINIMUM, 0x00),    //   LOGICAL_MINIMUM (0)
-    (LOGICAL_MAXIMUM, 0x01),    //   LOGICAL_MAXIMUM (1)
-    (REPORT_SIZE, 0x01),        //   REPORT_SIZE (1)
-    (REPORT_COUNT, 0x10),       //   REPORT_COUNT (16)
-    (USAGE, 0xB5),              //   USAGE (Scan Next Track)     ; bit 0: 1
-    (USAGE, 0xB6),              //   USAGE (Scan Previous Track) ; bit 1: 2
-    (USAGE, 0xB7),              //   USAGE (Stop)                ; bit 2: 4
-    (USAGE, 0xCD),              //   USAGE (Play/Pause)          ; bit 3: 8
-    (USAGE, 0xE2),              //   USAGE (Mute)                ; bit 4: 16
-    (USAGE, 0xE9),              //   USAGE (Volume Increment)    ; bit 5: 32
-    (USAGE, 0xEA),              //   USAGE (Volume Decrement)    ; bit 6: 64
-    (USAGE, 0x23, 0x02),        //   Usage (WWW Home)            ; bit 7: 128
-    (USAGE, 0x94, 0x01),        //   Usage (My Computer) ; bit 0: 1
-    (USAGE, 0x92, 0x01),        //   Usage (Calculator)  ; bit 1: 2
-    (USAGE, 0x2A, 0x02),        //   Usage (WWW fav)     ; bit 2: 4
-    (USAGE, 0x21, 0x02),        //   Usage (WWW search)  ; bit 3: 8
-    (USAGE, 0x26, 0x02),        //   Usage (WWW stop)    ; bit 4: 16
-    (USAGE, 0x24, 0x02),        //   Usage (WWW back)    ; bit 5: 32
-    (USAGE, 0x83, 0x01),        //   Usage (Media sel)   ; bit 6: 64
-    (USAGE, 0x8A, 0x01),        //   Usage (Mail)        ; bit 7: 128
-    (HIDINPUT, 0x02), // INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    (END_COLLECTION), // END_COLLECTION
-);
-
-pub const GAMEPAD_REPORT_DESCRIPTOR: &[u8] = hid!(
-    (USAGE_PAGE, 0x01), // USAGE_PAGE (Generic Desktop Ctrls)
-    (USAGE, 0x05),      // USAGE (Gamepad)
-    (COLLECTION, 0x01), // COLLECTION (Application)
+pub const HID_REPORT_DISCRIPTOR: &[u8] = hid!(
+    (0x05, 0x01), // USAGE_PAGE (Generic Desktop Ctrls)
+    (0x09, 0x04), // USAGE (Gamepad)
+    (0xa1, 0x01), // COLLECTION (Application)
     // ------------------------------------------------- Gamepad
-    (REPORT_ID, 0x01),       // REPORT_ID (1)
-    (USAGE_PAGE, 0x09),      // USAGE_PAGE (Button)
-    (USAGE_MINIMUM, 0x01),   // USAGE_MINIMUM (Button 1)
-    (USAGE_MAXIMUM, 0x80),   // USAGE_MAXIMUM (Button 16)
-    (LOGICAL_MINIMUM, 0x00), // LOGICAL_MINIMUM (0)
-    (LOGICAL_MAXIMUM, 0x08), // LOGICAL_MAXIMUM (1)
-    (REPORT_SIZE, 0x01),     // REPORT_SIZE (1)
-    (REPORT_COUNT, 0x08),    // REPORT_COUNT (5 buttons)
-    (HIDINPUT, 0x02),        // INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    // // ------------------------------------------------- Joystick Axes
-    // (USAGE_PAGE, 0x01),      // USAGE_PAGE (Generic Desktop Ctrls)
-    // (USAGE, 0x30),           // USAGE (X Axis)
-    // (USAGE, 0x31),           // USAGE (Y Axis)
-    // (USAGE, 0x32),           // USAGE (Z Axis)
-    // (LOGICAL_MINIMUM, 0x80), // LOGICAL_MINIMUM (-128)
-    // (LOGICAL_MAXIMUM, 0x7F), // LOGICAL_MAXIMUM (127)
-    // (REPORT_SIZE, 0x08),     // REPORT_SIZE (8 bits)
-    // (REPORT_COUNT, 0x03),    // REPORT_COUNT (3 axes: X, Y, Z)
-    // (HIDINPUT, 0x02),        // INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    // // ------------------------------------------------- D-pad (Hat Switch)
-    // (USAGE_PAGE, 0x01),      // USAGE_PAGE (Generic Desktop Ctrls)
-    // (USAGE, 0x39),           // USAGE (Hat Switch)
-    // (LOGICAL_MINIMUM, 0x00), // LOGICAL_MINIMUM (0)
-    // (LOGICAL_MAXIMUM, 0x08), // LOGICAL_MAXIMUM (8)
-    // (REPORT_SIZE, 0x04),     // REPORT_SIZE (4 bits)
-    // (REPORT_COUNT, 0x01),    // REPORT_COUNT (1 hat switch)
-    // (HIDINPUT, 0x02),        // INPUT (Data,Var,Abs,No Wrap,Linear,Preferred State,No Null Position)
-    // ------------------------------------------------- End Collection
-    (END_COLLECTION), // END_COLLECTION
+    //(0x85, 0x01), //     REPORT_ID (1)
+    // Buttons
+    (0x05, 0x09), //     USAGE_PAGE (Button)
+    (0x19, 0x01), //     USAGE_MINIMUM (First button 1)
+    (0x29, 0x08), //     USAGE_MAXIMUM (Last button 8)
+    (0x15, 0x00), //     LOGICAL_MINIMUM (Released 0)
+    (0x25, 0x01), //     LOGICAL_MAXIMUM (Pressed 1)
+    (0x75, 0x01), //     REPORT_SIZE (1 byte)
+    (0x95, 0x08), //     REPORT_COUNT (8 buttons per byte)
+    (0x81, 0x02), //     INPUT (Data,Var,Abs)
+    (0xc0),       // END_COLLECTION
 );
